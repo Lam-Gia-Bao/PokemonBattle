0be9d2161e1c746e502edec20e6449068b0d5786
Kuuhakuvn <lolphumobile@gmail.com>
2025-11-11 16:02:28 +0700

    Add files via upload

--- model/PokemonType.java
+++ model/PokemonType.java
@@ -7,7 +7,21 @@ package model;
  * Fire: hệ lửa
  * Water: hệ nước
  * Grass: hệ cỏ
+ * Ground: hệ đất
+ * Rock: hệ đá
+ * Fighting: hệ giác đấu
+ * Steel: hệ thép
+ * Fairy: hệ tiên
+ * Poison: hệ độc
+ * Bug: hệ bọ
+ * Dragon: hệ rồng
+ * Psyshic: hệ siêu linh
+ * Ice: hệ băng
+ * Ghost: hệ ma
+ * Dark: hệ bóng tối
  */
 public enum PokemonType {
-	ELECTRIC, FLYING, NORMAL, FIRE, WATER, GRASS;
+	ELECTRIC, FLYING, NORMAL, FIRE, WATER, GRASS, 
+	GROUND, FIGHTING, FAIRY, ROCK, STEEL, POISON,
+	BUG, DRAGON, ICE, PSYCHIC, GHOST, DARK;
 }
--- view/LoadImgView.java
+++ view/LoadImgView.java
@@ -13,9 +13,44 @@ public class LoadImgView extends JPanel {
 	
 	public LoadImgView() {
 		try {
-			backgroundImg = ImageIO.read(new File("./resources/background.png"));
-			playerImg = ImageIO.read(new File("./resources/pikachu_back.png"));
-			aiImg = ImageIO.read(new File("./resources/pidgey_front.png"));
+			backgroundImg = ImageIO.read(new File("src/resources/background.png"));
+			playerImg = ImageIO.read(new File("src/resources/pikachu_back.png"));
+			playerImg = ImageIO.read(new File("src/resources/suicune.png"));
+			playerImg = ImageIO.read(new File("src/resources/charizard.png"));
+			playerImg = ImageIO.read(new File("src/resources/greninja.png"));
+			playerImg = ImageIO.read(new File("src/resources/magearna.png"));
+			playerImg = ImageIO.read(new File("src/resources/haxorus.png"));
+			playerImg = ImageIO.read(new File("src/resources/sceptile.png"));
+			playerImg = ImageIO.read(new File("src/resources/scizor.png"));
+			playerImg = ImageIO.read(new File("src/resources/vileplume-f.png"));
+			playerImg = ImageIO.read(new File("src/resources/nidoking.png"));
+			playerImg = ImageIO.read(new File("src/resources/jirachi.png"));
+			playerImg = ImageIO.read(new File("src/resources/gyarados.png"));
+			playerImg = ImageIO.read(new File("src/resources/zekrom.png"));
+			playerImg = ImageIO.read(new File("src/resources/reshiram.png"));
+			playerImg = ImageIO.read(new File("src/resources/kyurem.png"));
+			playerImg = ImageIO.read(new File("src/resources/snorlax.png"));
+			playerImg = ImageIO.read(new File("src/resources/arceus-normal.png"));
+			playerImg = ImageIO.read(new File("src/resources/lapras.png"));
+			playerImg = ImageIO.read(new File("src/resources/drapion.png"));
+			playerImg = ImageIO.read(new File("src/resources/absol.png"));
+			playerImg = ImageIO.read(new File("src/resources/tyranitar.png"));
+			playerImg = ImageIO.read(new File("src/resources/archeops.png"));
+			playerImg = ImageIO.read(new File("src/resources/machamp.png"));
+			playerImg = ImageIO.read(new File("src/resources/lucario.png"));
+			playerImg = ImageIO.read(new File("src/resources/golem.png"));
+			playerImg = ImageIO.read(new File("src/resources/rhydon.png"));
+			playerImg = ImageIO.read(new File("src/resources/gardevoir.png"));
+			playerImg = ImageIO.read(new File("src/resources/volcarona.png"));
+			
+			aiImg = ImageIO.read(new File("src/resources/pidgey_front.png"));
+			aiImg = ImageIO.read(new File("src/resources/steelix.png"));
+			aiImg = ImageIO.read(new File("src/resources/gengar.png"));
+			aiImg = ImageIO.read(new File("src/resources/giratina-altered.png"));
+			aiImg = ImageIO.read(new File("src/resources/serperior.png"));
+			aiImg = ImageIO.read(new File("src/resources/diancie.png"));
+			aiImg = ImageIO.read(new File("src/resources/mewtwo.png"));
+			aiImg = ImageIO.read(new File("src/resources/rayquaza.png"));
 		} catch (Exception e) {
 			System.out.println("Error loading images: " + e.getMessage());
 		}
